ce99999be7ffc0d5992ebdb1bf77c2f82e812bbe
Laneone <dude.1996@live.com>
2015-04-14 21:49:30 +0530

    updated answer retrieval

--- pull_answers.py
+++ pull_answers.py
@@ -9,53 +9,52 @@ from askfm import *
 
 if(len(sys.argv)==0):
     exit(0)
-username = "LokeshPoovaragan"
-question_list = []
-asked_by_list = []
-answer_list = []
-img_reply_bool_list = []
-img_reply_src_list = []
-like_count_list = []
+username = sys.argv[1]
+tuple_holder = []
 tree = getTree(username)
 for i in tree.xpath("//div[@id='common_question_container']/div"):
-	for j in i.xpath("div/span/span"):
-		question_list.append(" ".join(j.text.split('\n')))
-for i in tree.xpath("//div[@id='common_question_container']/div"):
-	asked_by = i.find('div/span/a')
-	if(asked_by == None):
-		asked_by_list.append("Anonymous")
-	else:
-		asked_by_list.append(asked_by.text)
-for i in tree.xpath("//div[@id='common_question_container']/div"):
-	for j in i.xpath("div[3]"):
-		answer_list.append(' '.join(j.itertext()).strip())
-for i in tree.xpath("//div[@id='common_question_container']/div"):
-	for j in i.xpath("div[3]"):
-		nodes = j.getchildren()
-		img_reply = False
-		for k in nodes:
-			if(k.tag == 'a'):
-				img_reply = True
-		img_reply_bool_list.append(img_reply)
-for i in tree.xpath("//div[@id='common_question_container']/div"):
-	for j in i.xpath("div[3]"):
-		nodes = j.getchildren()
-		for k in nodes:
-			if(k.tag == 'a'):
-				imgTree = html.fromstring(requests.get("http://ask.fm" + k.get('href')).text)
-				img_reply_src_list.append(imgTree.xpath("//img[@id='nopup-picture']")[0].get('src'))
-			else:
-				img_reply_src_list.append(None)
-for i in tree.xpath("//div[@id='common_question_container']/div"):
-	for j in i.xpath("div[5]/div[2]"):
-		nodes = j.getchildren()
-		if(len(nodes) == 0):
-			like_count_list.append(None)
-		else:
-			like_count_list.append(nodes[0].text.split(' ')[0])
-for i,j,k,l,m,n in zip(question_list, asked_by_list, answer_list, img_reply_bool_list, img_reply_src_list, like_count_list):
-    #print(i + "," + j + "," + k + "," + str(l) + "," + str(m) + "," + str(n))
-    print(j + "," + str(l) + "," + str(m))
-    
+    #question
+    for j in i.xpath("div/span/span"):
+        question = (" ".join(j.text.split('\n')))
+    #asked_by_who
+    asked_by = i.find('div/span/a')
+    if(asked_by == None):
+        asked_by_who = "Anonymous"
+    else:
+        asked_by_who = asked_by.text
+    #answer
+    for j in i.xpath("div[3]"):
+        answer = (' '.join(j.itertext()).strip())
+    #img_reply_bool
+    for j in i.xpath("div[3]"):
+        nodes = j.getchildren()
+        img_reply = False
+        for k in nodes:
+            if(k.tag == 'a'):
+                img_reply = True
+        img_reply_bool = (img_reply)
+    #img_reply_src
+    for j in i.xpath("div[3]"):
+        nodes = j.getchildren()
+        img_reply_src = (None)
+        for k in nodes:
+            if(k.tag == 'a'):
+                imgTree = html.fromstring(requests.get("http://ask.fm" + k.get('href')).text)
+                img_reply_src = (imgTree.xpath("//img[@id='nopup-picture']")[0].get('src'))
+    #like_count
+    for j in i.xpath("div[5]/div[2]"):
+        nodes = j.getchildren()
+        if(len(nodes) == 0):
+            like_count = (None)
+        else:
+            like_count = (nodes[0].text.split(' ')[0])
 
-		
+    tuple_holder.append((question, asked_by_who, answer, img_reply_bool, img_reply_src, like_count))
+
+for i in tuple_holder:
+    print("Question: " + i[0])
+    print("Asked By: " + i[1])
+    print("Answer: " + i[2])
+    print("Is Image Reply?: " + str(i[3]))
+    print("Image Source: " + str(i[4]))
+    print("Likes: " + str(i[5]))
