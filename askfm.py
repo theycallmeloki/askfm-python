565c6f2f9becdcd86182a1705fc4587c9c21a00c
Laneone <dude.1996@live.com>
2015-04-28 01:13:30 +0530

    updated answer retriever

--- askfm.py
+++ askfm.py
@@ -39,55 +39,15 @@ def getBio(tree):
 
 def getAnswerCount(tree):
     return tree.xpath("//*[@id='profile_answer_counter']")[0].text
-    
+
 def getLikeCount(tree):
     return tree.xpath("//*[@id='profile_liked_counter']")[0].text
 
 def getGifts(tree):
     return tree.xpath("//*[@id='profile_gifts_counter']")[0].text
 
-def get_like_urls(tree, username):
-    likes_urls = []
-    usernames = []
-    
-    for i in tree.xpath("//div[@id='common_question_container']/div"):
-        for j in i.xpath("div[5]/div[2]/a"):
-            print("http://ask.fm" + j.get("href"))
-            likes_urls.append("http://ask.fm" + j.get("href"))
-            
-    if(len(tree.xpath("//*[@id='more-container']")) == 0):
-        return likes_urls
-
-    next_page = -1  
-    while(True):
-        token = getToken(tree)
-        time = getTime(tree)        
-        if(next_page < 0):
-            page = getPage(tree)
-        else:
-            page = next_page
-        data = {
-            "time":time,
-            "page":page,
-            "authenticity_token":token
-        }
-        page_plus_one = requests.post("http://ask.fm/" + username + "/more",params=data)
-        if(page_plus_one.text == "$(\"#more-container\").hide();"):
-            break
-        reg_html_page_plus_one = re.search(r'after\("((?:[^"]+|\\")+)"\)', page_plus_one.text)
-        html_page_plus_one = bytes(reg_html_page_plus_one.group(1), "utf-8").decode("unicode_escape")
-        tree_page_plus_one = html.fromstring(html_page_plus_one)
-        for i in tree_page_plus_one[1]:
-            for j in i.xpath("div[5]/div[2]/a"):
-                print("http://ask.fm" + j.get("href"))
-                likes_urls.append("http://ask.fm" + j.get("href"))
-        next_page = int(re.search(r'\d+', re.search(r'val\(\d+\)', page_plus_one.text).group(0)).group(0))
-    
-    return likes_urls
-
-def returnUsernames(like_url):
+def getUsernames(like_url):
     username_list = []
-    print("From answer: " + like_url)
     tree = html.fromstring(requests.get(like_url).text)
     username_list = username_list + extractUsernames(tree)
     pagination = tree.get_element_by_id('page-container', None)
@@ -99,7 +59,6 @@ def returnUsernames(like_url):
                 pages.append(int(i.text))
         last_page_of_pagination = (max(pages)+1)
         for a_page in range(2,last_page_of_pagination):
-            print("Pagination: " + str(a_page) + " of " + str((last_page_of_pagination)-1))
             like_tree = html.fromstring(requests.get(like_url + "?page=" + str(a_page)).text)
             username_list = username_list + extractUsernames(like_tree)
     return username_list
@@ -108,26 +67,94 @@ def extractUsernames(like_tree):
     extract_list = []
     for i in like_tree.xpath("//*[@id=\"wrapper\"]/div/div"):
             for j in i.xpath("div/div[2]/span/span"):
-                #print("  " + j.text)
                 extract_list.append(j.text)
     return extract_list
-    
-    
-def getUsernames(likes_urls):
-    usernames = []
-    i = 0
-    while i < len(likes_urls):
-        try:
-            print("Getting likes: " + str(i+1) + "  of " + str(len(likes_urls)) + " | " + str(round((((i+1) / (len(likes_urls))) * 100),2))  + " Percentage complete")
-            returned_usernames = returnUsernames(likes_urls[i])
-            print("Got " + str(len(returned_usernames)) + " usernames")
-            usernames = usernames + returned_usernames
-            i += 1
-        except KeyboardInterrupt:
-            exit(0)
-        except:
-            print("Failed: " + str(i))
-            sleep_time = 5
-            print("Sleeping for: " + str(sleep_time) + " seconds")
-            sleep(sleep_time)
-    return set(usernames)
+
+def responseSorter(question):
+    #question_text
+    for j in question.xpath("div/span/span"):
+        question_text = ((" ".join(j.text.split('\n'))).encode('ascii', 'ignore').decode())
+    #asked_by_who
+    asked_by = question.find('div/span/a')
+    if(asked_by == None):
+        asked_by_who = "Anonymous"
+    else:
+        asked_by_who = "@" + ((asked_by.get('href').lstrip('/')).encode('ascii', 'ignore').decode())
+    #answer
+    for j in question.xpath("div[3]"):
+        answer = ((' '.join(j.itertext()).strip()).encode('ascii', 'ignore').decode())
+    #img_reply_bool
+    for j in question.xpath("div[3]"):
+        nodes = j.getchildren()
+        img_reply = False
+        for k in nodes:
+            if(k.tag == 'a'):
+                img_reply = True
+        img_reply_bool = (img_reply)
+    #img_reply_src
+    for j in question.xpath("div[3]"):
+        nodes = j.getchildren()
+        img_reply_src = (None)
+        for k in nodes:
+            if(k.tag == 'a'):
+                imgTree = html.fromstring(requests.get("http://ask.fm" + k.get('href')).text)
+                img_reply_src = (imgTree.xpath("//img[@id='nopup-picture']")[0].get('src'))
+    #like_url
+    like_url = (None)
+    for j in question.xpath("div[5]/div[2]/a"):
+        like_url = ("http://ask.fm" + j.get("href"))
+    #like_count
+    for j in question.xpath("div[5]/div[2]"):
+        nodes = j.getchildren()
+        if(len(nodes) == 0):
+            like_count = (None)
+        else:
+            like_count = (nodes[0].text.split(' ')[0])
+    #like_list
+    like_list= (None)
+    if(like_url != None):
+        like_list = getUsernames(like_url)
+
+    return_data = {
+        "question_text":question_text,
+        "asked_by_who":asked_by_who,
+        "answer":answer,
+        "img_reply_bool":img_reply_bool,
+        "img_reply_src":img_reply_src,
+        "like_url":like_url,
+        "like_count":like_count,
+        "like_list":like_list
+    }
+    return return_data
+
+def getAnswers(username):
+    dict_holder = []
+    tree = getTree(username)
+    for i in tree.xpath("//div[@class='questionBox']"):
+        dict_holder.append(responseSorter(i))
+
+    if(len(tree.xpath("//*[@id='more-container']")) == 0):
+        return dict_holder
+
+    next_page = -1
+    while(True):
+        token = getToken(tree)
+        time = getTime(tree)
+        if(next_page < 0):
+            page = getPage(tree)
+        else:
+            page = next_page
+        data = {
+            "time":time,
+            "page":page,
+            "authenticity_token":token
+        }
+        raw_post_result = requests.post("http://ask.fm/" + username + "/more",params=data)
+        if(raw_post_result.text == "$(\"#more-container\").hide();"):
+            break
+        js_post_result = re.search(r'after\("((?:[^"]+|\\")+)"\)', raw_post_result.text)
+        tree_next_page = html.fromstring(bytes(js_post_result.group(1), "utf-8").decode("unicode_escape"))
+        for i in tree_next_page[1]:
+            dict_holder.append(responseSorter(i))
+        next_page = int(re.search(r'\d+', re.search(r'val\(\d+\)', raw_post_result.text).group(0)).group(0))
+    return dict_holder
--- pull_answers.py
+++ pull_answers.py
@@ -9,56 +9,15 @@ from askfm import *
 
 if(len(sys.argv)==0):
     exit(0)
-username = sys.argv[1]
-tuple_holder = []
-tree = getTree(username)
-for i in tree.xpath("//div[@id='common_question_container']/div"):
-    #question
-    for j in i.xpath("div/span/span"):
-        question = (" ".join(j.text.split('\n')))
-    #asked_by_who
-    asked_by = i.find('div/span/a')
-    if(asked_by == None):
-        asked_by_who = "Anonymous"
-    else:
-        asked_by_who = "@" + (asked_by.get('href').lstrip('/'))
-    #answer
-    for j in i.xpath("div[3]"):
-        answer = (' '.join(j.itertext()).strip())
-    #img_reply_bool
-    for j in i.xpath("div[3]"):
-        nodes = j.getchildren()
-        img_reply = False
-        for k in nodes:
-            if(k.tag == 'a'):
-                img_reply = True
-        img_reply_bool = (img_reply)
-    #img_reply_src
-    for j in i.xpath("div[3]"):
-        nodes = j.getchildren()
-        img_reply_src = (None)
-        for k in nodes:
-            if(k.tag == 'a'):
-                imgTree = html.fromstring(requests.get("http://ask.fm" + k.get('href')).text)
-                img_reply_src = (imgTree.xpath("//img[@id='nopup-picture']")[0].get('src'))
-    #like_url
-    like_url = (None)
-    for j in i.xpath("div[5]/div[2]/a"):
-        like_url = ("http://ask.fm" + j.get("href"))
-    #like_count
-    for j in i.xpath("div[5]/div[2]"):
-        nodes = j.getchildren()
-        if(len(nodes) == 0):
-            like_count = (None)
-        else:
-            like_count = (nodes[0].text.split(' ')[0])
-    tuple_holder.append((question, asked_by_who, answer, img_reply_bool, img_reply_src, like_url, like_count))
 
-for i in tuple_holder:
-    print("Question: " + i[0])
-    print("Asked By: " + i[1])
-    print("Answer: " + i[2])
-    print("Is Image Reply?: " + str(i[3]))
-    print("Image Source: " + str(i[4]))
-    print("Like URL: " + str(i[5]))
-    print("Like Count: " + str(i[6]))
+username = sys.argv[1]
+dicts = getAnswers(username)
+for i in dicts:
+    print("Question: " + i["question_text"])
+    print("Asked By: " + i["asked_by_who"])
+    print("Answer: " + i["answer"])
+    print("Is Image Reply?: " + str(i["img_reply_bool"]))
+    print("Image Source: " + str(i["img_reply_src"]))
+    print("Like URL: " + str(i["like_url"]))
+    print("Like Count: " + str(i["like_count"]))
+    print("Like List:" + str(i["like_list"]))
